eb4b2a206602eb096abbf01a4ff84bc4040708dd
ndrtte <andreamorelaleman@gmail.com>
2026-02-13 22:12:25 -0600

    first commit

---
+++ gestiontickets/src/main/java/com/crm/gestiontickets/GestionticketsApplication.java
@@ -0,0 +1,13 @@
+package com.crm.gestiontickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionticketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionticketsApplication.class, args);
+	}
+
+}
---
+++ gestiontickets/src/test/java/com/crm/gestiontickets/GestionticketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.gestiontickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionticketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
