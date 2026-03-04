ec52b21f20d0dfff1552c577ec71aa24af93f473
ndrtte <andreamorelaleman@gmail.com>
2026-03-03 22:17:36 -0600

    Servicio para credenciales

---
+++ gestiontickets/src/main/java/com/crm/gestiontickets/service/AgenteService.java
@@ -0,0 +1,16 @@
+package com.crm.gestiontickets.service;
+
+import org.springframework.stereotype.Service;
+
+import com.crm.gestiontickets.dto.SolicitudLogin;
+
+@Service
+public class AgenteService {
+    
+    public String inicioSesion (SolicitudLogin credenciales){
+        
+
+        return "";
+    }
+
+}
