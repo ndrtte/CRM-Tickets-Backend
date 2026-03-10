1534ee4c3231a97bb95ab5d772b56ac8f8dfe6c4
ndrtte <andreamorelaleman@gmail.com>
2026-03-10 00:05:07 -0600

    Mejoras a ticketDetalle. Tengo que ordernar mejor los dtos antes de que se me haga un desorden...

---
+++ gestiontickets/src/main/java/com/crm/gestiontickets/dto/EtapaTicket.java
@@ -0,0 +1,16 @@
+package com.crm.gestiontickets.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EtapaTicket {
+    private Integer idPaso;
+    private String descripcion;
+    private boolean esActual;
+}
