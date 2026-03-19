64699285511588c8800377eb4e86f1c9bd898905
ndrtte <andreamorelaleman@gmail.com>
2026-03-19 00:23:26 -0600

    Le quite implementacion innecesaria

--- gestiontickets/src/main/java/com/crm/gestiontickets/enums/EstadoEtapaTicket.java
+++ gestiontickets/src/main/java/com/crm/gestiontickets/enums/EstadoEtapaTicket.java
@@ -5,14 +5,8 @@ import lombok.Getter;
 @Getter
 public enum EstadoEtapaTicket {
 
-    NO_INICIADO("No Iniciado"),
-    EN_PROCESO("En Proceso"),
-    FINALIZADO("Finalizado");
-
-    private final String descripcion;
-
-    EstadoEtapaTicket(String descripcion) {
-        this.descripcion = descripcion;
-    }
+    NO_INICIADO,
+    EN_PROCESO,
+    FINALIZADO
 
 }
