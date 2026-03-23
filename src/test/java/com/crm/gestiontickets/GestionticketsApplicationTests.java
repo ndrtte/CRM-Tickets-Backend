4de44383be7f973eaa0de99418f3d4642cfde66a
ndrtte <andreamorelaleman@gmail.com>
2026-03-22 18:26:51 -0600

    añadiendo configuraciones para el pr

--- src/test/java/com/crm/gestiontickets/GestionticketsApplicationTests.java
+++ src/test/java/com/crm/gestiontickets/GestionticketsApplicationTests.java
@@ -2,7 +2,9 @@ package com.crm.gestiontickets;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class GestionticketsApplicationTests {
 
