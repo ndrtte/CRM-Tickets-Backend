0893af5db7e0af878c5e9a3830cf08e0f68d7bf1
ndrtte <andreamorelaleman@gmail.com>
2026-04-10 03:18:56 -0600

    sin web security

--- src/main/java/com/crm/gestiontickets/config/SecurityConfig.java
+++ src/main/java/com/crm/gestiontickets/config/SecurityConfig.java
@@ -2,12 +2,10 @@ package com.crm.gestiontickets.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
